0235f8083fe289f505a10160d780482f7a6e9192
Illzen <908413385@qq.com>
2025-04-23 21:58:20 +0800

    update

--- copy.sh
+++ copy.sh
@@ -1,8 +1,10 @@
-rm -rf ~/.config/rofi
 rm -rf ~/.config/nvim 
-ln -snf ./.config/rofi ~/.config/
 ln -snf ./.config/nvim ~/.config/
 ln -snf ./.config/starship.toml ~/.config/starship.toml
 ln -snf ./.tmux.conf ~/.tmux.conf
 ln -snf ./.zshrc ~/.zshrc
 cp ./.gitconfig ~/.gitconfig
+rm -rf ~/.tmux
+mkdir ~/.tmux/
+mkdir ~/.tmux/plugins/
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
